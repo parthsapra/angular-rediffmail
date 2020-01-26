7412c7f5338847708475149268cd9016caeb12af
parthsapra <parthsapra91@gmail.com>
2020-01-26 18:11:48 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import {RediffUser} from './rediffuser.model';
+import { Component } from "@angular/core";
+import { RediffUser } from "./rediffuser.model";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Create a Rediffmail account';
-  user:RediffUser=new RediffUser();
-  submit(){
+export class AppComponent {
+  name = "Create a Rediffmail account";
+  user: RediffUser = new RediffUser();
+  submit() {
     console.log(this.user);
   }
 }
-
